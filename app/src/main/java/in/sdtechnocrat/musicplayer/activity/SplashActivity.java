6addebd91f1a8e97876e44cf89b3fd62dc36c72b
srdalai <satyaranjandalai013@gmail.com>
2019-09-19 00:53:09 +0530

    Splash Screen Added

---
+++ app/src/main/java/in/sdtechnocrat/musicplayer/activity/SplashActivity.java
@@ -0,0 +1,105 @@
+package in.sdtechnocrat.musicplayer.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.PathParser;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import in.sdtechnocrat.musicplayer.R;
+import in.sdtechnocrat.musicplayer.utils.BitmapUtils;
+
+public class SplashActivity extends AppCompatActivity {
+
+    int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
+
+    ImageView imageView;
+
+    Animation rotation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imageView = findViewById(R.id.imageView);
+
+        rotation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        rotation.setFillAfter(true);
+
+        imageView.setImageBitmap(convertToHeart(BitmapFactory.decodeResource(getResources(), R.drawable.album_art)));
+
+        imageView.startAnimation(rotation);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            } else {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                    }
+                }, 4000);
+            }
+        }
+
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Other";
+            String description = "Other";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel("10", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+    private Bitmap convertToHeart(Bitmap src) {
+        return BitmapUtils.getCroppedBitmap(src, getHeartPath(src));
+    }
+
+    private Path getHeartPath(Bitmap src) {
+        return resizePath(PathParser.createPathFromPathData(getString(R.string.albumN)),
+                src.getWidth(), src.getHeight());
+    }
+
+    public static Path resizePath(Path path, float width, float height) {
+        RectF bounds = new RectF(0, 0, width, height);
+        Path resizedPath = new Path(path);
+        RectF src = new RectF();
+        resizedPath.computeBounds(src, true);
+
+        Matrix resizeMatrix = new Matrix();
+        resizeMatrix.setRectToRect(src, bounds, Matrix.ScaleToFit.CENTER);
+        resizedPath.transform(resizeMatrix);
+
+        return resizedPath;
+    }
+}
